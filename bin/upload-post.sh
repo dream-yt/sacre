e59b53f14c071d25add436b8a15d30ed18db1483
sakamossan <mild7caloriemategreentea@gmail.com>
2020-04-30 18:02:35 +0900

    shell-for-parallel

--- bin/upload-post.sh
+++ bin/upload-post.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -eux
 cd $(dirname $0)/../
+readonly post_title=$(cat /tmp/post_title)
 
 git pull --ff-only origin master
 (cd public; git pull --ff-only origin master)
-
-readonly post_title=$(cat /tmp/post_title)
 hugo
 
 pushd public
@@ -19,6 +18,6 @@ git commit -m $post_title
 git push origin master
 
 pushd ~/.ghq/github.com/sakamossan/services/blog.n-t.jp
-    cp ~/.ghq/github.com/dream-yt/sacre/content/post/$post_title.md content/post/
+    cp ~/.ghq/github.com/dream-yt/sacre/content/post/$post_title.md content/
     scripts/deploy.sh
 popd
---
+++ content/post/shell-for-parallel.md
@@ -0,0 +1,32 @@
+---
+title: "bashのfor文の中身を並列処理させる"
+date: 2020-04-30T18:01:37+09:00
+draft: false
+---
+
+こんなスクリプトがあったとして
+
+```sh
+for i ($find ./src); do
+  somecommand $i
+done
+```
+
+forの中身 (somecommand) を並列で実行したい場合はこうすればよい
+
+```sh
+for i ($find ./src); do
+  somecommand $i &
+done
+wait
+```
+
+いままでxargsを使ったりもしてたが、waitを使う方が分かりやすくてよかった
+
+
+## 参考
+
+- [waitコマンド（子プロセスの完了を待つ） : JP1/Advanced Shell](http://itdoc.hitachi.co.jp/manuals/3021/3021313320/JPAS0399.HTM)
+
+> pidを指定しなかった場合は，実行中のすべての子プロセスの完了を待ちます
+
